43a0c771fa807f268fa00d36fc8ae6a4a3d5bef5
Kiid <kiidtyga@gmail.com>
2020-05-23 16:36:24 +0200

    Update SpellDataBase.cs

--- Player/Spells/SpellDataBase.cs
+++ Player/Spells/SpellDataBase.cs
@@ -54,7 +54,7 @@ namespace ChampionsOfForest.Player
                 aim = SpellActions.DoBlinkAim,
                 
             };
-            new Spell(4, 120, 10, 100, 45, "Sun Flare", "A light column heals players inside and gives them +25% movement speed, while slowing damaging enemies. Slow amount is equal to 25%")
+            new Spell(4, 120, 10, 100, 45, "Eruption", "A light column heals players inside and gives them +25% movement speed, while slowing damaging enemies. Slow amount is equal to 25%")
             {
                 active = SpellActions.CastFlare,
                 
@@ -70,11 +70,11 @@ namespace ChampionsOfForest.Player
             {
                 active = AutoPickupItems.DoPickup,            
             };
-            new Spell(7, 115, 17, 25, 2f, "Black Flame", "Ignites your weapon with a dark flame that empowers all attacks.")
+            new Spell(7, 115, 17, 25, 2f, "Schwarze Flamme", "Ignites your weapon with a dark flame that empowers all attacks.")
             {
                 active =BlackFlame.Toggle,            
             };
-            new Spell(8,123, 12, 80, 110, "War Cry", "Empowers you and nearby allies for 2 minutes.")
+            new Spell(8,123, 12, 80, 110, "Kriegsschrei", "Empowers you and nearby allies for 2 minutes.")
             {
                 active =SpellActions.CastWarCry,            
             };
@@ -82,11 +82,11 @@ namespace ChampionsOfForest.Player
             {
                 active =SpellActions.CastPortal,            
             };
-            new Spell(10, 125, 30, 90, 20, "Magic Arrow", "A large arrow is shot where you're looking at. Slows any enemies on hit and deals big damage")
+            new Spell(10, 125, 30, 90, 20, "Verzauberter Pfeil", "A large arrow is shot where you're looking at. Slows any enemies on hit and deals big damage")
             {
                 active =SpellActions.CastMagicArrow,            
             };
-            new Spell(11, 127, 35, 10, 10, "Multishot", "An attack modifier. Enchants your ranged weapons to shoot multipe projectiles. Upgradeable by perks. Energy is consumed upon firing and depends on the amount of projectiles fired")
+            new Spell(11, 127, 35, 10, 10, "Multischuss", "An attack modifier. Enchants your ranged weapons to shoot multipe projectiles. Upgradeable by perks. Energy is consumed upon firing and depends on the amount of projectiles fired")
             {
                 active =SpellActions.ToggleMultishot,            
             };
@@ -94,7 +94,7 @@ namespace ChampionsOfForest.Player
             {
                 active =Gold.Cast,            
             };
-            new Spell(13, 132, 7, 40, 15, "Purge", "Everyone in your surroudings gets cleansed of their negative debuffs. Negates poison.")
+            new Spell(13, 132, 7, 40, 15, "Säuberung", "Everyone in your surroudings gets cleansed of their negative debuffs. Negates poison.")
             {
                 active =SpellActions.CastPurge,            
             };
@@ -102,11 +102,11 @@ namespace ChampionsOfForest.Player
             {
                 active =SpellActions.CastSnapFreeze,            
             };
-            new Spell(15, 131, 25, 15, 180, "Berserk", "For short amount of time, gain damage, attack speed and movement speed, take additional damage and have unlimited stamina.")
+            new Spell(15, 131, 25, 15, 180, "Berserker", "For short amount of time, gain damage, attack speed and movement speed, take additional damage and have unlimited stamina.")
             {
                 active =Berserker.Cast,            
             };
-            new Spell(16, 130, 42, 300, 90, "Ball Lightning", "A slow moving, bouncing ball of lightning travels forward, dealing damage to hit enemies, and upon contact or when it lasts too long, it explodes. Scales with 320% spell damage.")
+            new Spell(16, 130, 42, 300, 90, "Lichtkugel", "A slow moving, bouncing ball of lightning travels forward, dealing damage to hit enemies, and upon contact or when it lasts too long, it explodes. Scales with 320% spell damage.")
             {
                 active =SpellActions.CastBallLightning,            
             };
@@ -118,23 +118,23 @@ namespace ChampionsOfForest.Player
             {
                 passive = x=> SpellActions.Frenzy = x,            
             };
-            new Spell(19, 135, 27, 40, 10, "Seeking Arrow", "Casting spell empowers arrow, causing all arrows to head in the same direction for 30 seconds. While active, arrows deal more damage, the further target they hit, headshots deal double damage and bodyshots slow enemies by 20% for 4 seconds.")
+            new Spell(19, 135, 27, 40, 10, "Suchender Pfeil", "Casting spell empowers arrow, causing all arrows to head in the same direction for 30 seconds. While active, arrows deal more damage, the further target they hit, headshots deal double damage and bodyshots slow enemies by 20% for 4 seconds.")
             {
                 active = SpellActions.SeekingArrow_Active,
             };
-            new Spell(20, 137, 4, 0, 1, "Focus", "Passively, when landing a headshot, next projectile will deal 100% more damage and slow the enemy by 20%. When landing a body shot, next projectile will deal only 20% more damage, but attack speed is increased.")
+            new Spell(20, 137, 4, 0, 1, "Fokus", "Passively, when landing a headshot, next projectile will deal 100% more damage and slow the enemy by 20%. When landing a body shot, next projectile will deal only 20% more damage, but attack speed is increased.")
             {
                 passive = x => SpellActions.Focus = x,
             };
-            new Spell(21, 140, 8, 0, 1, "Parry", "Passively, when parrying an enemy, deal magic damage to enemies around the target. Additionally, gain energy, heal yourself for a small amount and get stun immunity for 10 seconds after parrying.")
+            new Spell(21, 140, 8, 0, 1, "Parieren", "Passively, when parrying an enemy, deal magic damage to enemies around the target. Additionally, gain energy, heal yourself for a small amount and get stun immunity for 10 seconds after parrying.")
             {
                 passive = x => SpellActions.Parry = x,
             };
-            new Spell(22, 141, 50, 400, 300, "Cataclysm", "Creates a fire tornado that ignites enemies, slows them and deals damage.")
+            new Spell(22, 141, 50, 400, 300, "Cataclysmus", "Creates a fire tornado that ignites enemies, slows them and deals damage.")
             {
                 active = ()=> SpellActions.CastCataclysm(),
             };
-            new Spell(23, 165, 11, 75, 8, "Blood Infused Arrow", "Sacrifice your own vitals to empower your next arrow. Drains percent of your current health and adds lost health as damage.")
+            new Spell(23, 165, 11, 75, 8, "Blutgetränkte Pfeile", "Sacrifice your own vitals to empower your next arrow. Drains percent of your current health and adds lost health as damage.")
             {
                 active = ()=> SpellActions.CastBloodInfArr(),
             };
